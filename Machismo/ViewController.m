dc64ac5d53712425248e5349ff6cdc001d9ec305
Linwei Ding <linweiding@hy-vmtest.marvell.com>
2015-06-05 14:14:46 -0700

    Initial Commit

---
+++ Machismo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Machismo
+//
+//  Created by Linwei Ding on 6/5/15.
+//  Copyright (c) 2015 linweidi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Machismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Machismo
+//
+//  Created by Linwei Ding on 6/5/15.
+//  Copyright (c) 2015 linweidi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
